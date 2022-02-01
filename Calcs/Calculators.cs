205ae7186295fc15620f6ddaf26fa691c96879c9
MUKILTEO\1710220 <griffindbarlow@gmail.com>
2022-02-01 11:45:38 -0800

    Add project files.

---
+++ Calcs/Calculators.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calcs
+{
+    public class Calculators
+    {
+        public string Name;
+        public string Color;
+        public string CoverColor;
+        public float PercentOfBattery;
+        public float Weight;
+        public int NumberOfColors;
+        public bool IsActive;
+        public bool IsSolar;
+        public int AmountBattries;
+        public string TypeOfCalc;
+        //Constructor
+
+
+
+        public Calculators()
+        {
+
+        }
+
+        public Calculators(string name, string color, string coverColor, float percentOfBattery, float weight, int numberOfColors, bool isActive, bool isSolar, int amountBattries, string typeOfCalc)
+        {
+            Name = name;
+            Color = color;
+            CoverColor = coverColor;
+            PercentOfBattery = percentOfBattery;
+            Weight = weight;
+            NumberOfColors = numberOfColors;
+            IsActive = isActive;
+            IsSolar = isSolar;
+            AmountBattries = amountBattries;
+            TypeOfCalc = typeOfCalc;
+        }
+
+        //Methods
+        public void Describe()
+        {
+            Console.WriteLine("\nCalc Name: " + Name);
+            Console.WriteLine("\nPercent Of Battry: " + PercentOfBattery + "%");
+            Console.WriteLine(" ");
+
+
+
+
+        }
+        //Method 2
+        public void Describe2()
+        {
+            Console.WriteLine("\tType Of calc: " + TypeOfCalc);
+            Console.WriteLine("\tIs Active: " + IsActive);
+        }
+
+        //Method 3
+        public void AddTime()
+        {
+            Console.WriteLine("Time Passes...");
+            if (PercentOfBattery <= 0)
+            {
+                IsActive = false;
+            }
+            if (IsActive == true)
+            {
+                PercentOfBattery--;
+            }
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ Calcs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+
+            //First Calc
+
+            Calculators TexasI = new Calculators("JakesCalc", "Fucia", "Lightish Red", 6, 2, 17, true, false, 2, "Eng");
+            //Console.WriteLine(TexasI.Name + " Is " + TexasI.Color + " With a " + TexasI.CoverColor + " Cover.");
+            //Console.WriteLine(TexasI.AmountBattries +" Batteries "+ TexasI.PercentOfBattery +"%"+ " is active " + TexasI.IsActive + " is solar " + TexasI.IsSolar);
+
+            Calculators RileyCalc = new Calculators("Riley's Calculator", "Black", "Black", 50, 2, 2, false, false, 2, "Texas Instruments");
+
+
+            TexasI.Describe();
+            TexasI.Describe2();
+            TexasI.AddTime();
+            TexasI.AddTime();
+            TexasI.AddTime();
+            TexasI.AddTime();
+            TexasI.Describe();
+            TexasI.Describe2();
+            TexasI.AddTime();
+            TexasI.AddTime();
+            TexasI.AddTime();
+            TexasI.Describe();
+            TexasI.Describe2();
+
+            RileyCalc.Describe();
+            RileyCalc.Describe2();
+            RileyCalc.AddTime();
+            RileyCalc.AddTime();
+            RileyCalc.Describe();
+            RileyCalc.Describe2();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
